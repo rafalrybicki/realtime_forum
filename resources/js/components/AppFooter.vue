4be491b0180844950636e75a60816afc93967072
Rafa <rafarybicki@gmail.com>
2020-09-10 21:13:58 +0200

    Add Vue, Router and Vuetify

---
+++ resources/js/components/AppFooter.vue
@@ -0,0 +1,13 @@
+<template>
+   <v-footer>
+      <v-spacer>Realtime Forum</v-spacer>
+      <div>&copy; {{ new Date().getFullYear() }}</div>
+   </v-footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ resources/js/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <Fragment>
+        <Toolbar />
+        <AppFooter />
+    </Fragment>
+</template>
+
+<script>
+import Toolbar from "./Toolbar";
+import AppFooter from "./AppFooter";
+import { Fragment } from "vue-fragment";
+export default {
+    components: { Toolbar, AppFooter, Fragment }
+};
+</script>
+
+<style></style>
